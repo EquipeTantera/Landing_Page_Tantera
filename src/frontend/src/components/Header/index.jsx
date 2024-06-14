d6fe356a6fd81f4a7e9418340457f8c941e8bef2
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-06-14 11:09:26 -0300

    feat: add header mobile

--- src/frontend/src/components/Header/index.jsx
+++ src/frontend/src/components/Header/index.jsx
@@ -1,13 +1,41 @@
 import styles from './styles.module.scss';
-import PropType from 'prop-types';
-
+import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 
 export default function Header({ background }) {
-  return ( 
-    <header className={styles.header}>
+  const [scroll, setScroll] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 50) {
+      setScroll(true);
+    } else {
+      setScroll(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return ReactDOM.createPortal(
+    <header className={`${styles.header} ${scroll ? styles.scrolled : ''} ${menuOpen ? styles.menuActive : ''}`}>
       <div className={styles.header__container}>
         <img className={styles.header__container__imagem} src="/logo_header.png"/>
       </div>
+      <input type="checkbox" id="menuToggle" className={styles.header__menu} onChange={toggleMenu}/>
+      <label htmlFor="menuToggle" className={styles.header__menu__label}>
+        <img src="/menu.svg" alt='Menu'/>
+      </label>
       <nav className={styles.header__nav}>
         <ul className={styles.header__nav__list}>
           <li className={styles.header__nav__list__item}>
@@ -27,10 +55,10 @@ export default function Header({ background }) {
           </li>
         </ul>
       </nav>
-    </header>
-  )
+    </header>, document.getElementById('header')
+  );
 }
 
 Header.propTypes = {
-  background: PropType.string,
+  background: PropTypes.string,
 };

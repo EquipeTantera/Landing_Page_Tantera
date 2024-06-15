c4c3f8cacea68252130c378a78390097c7dc4d79
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-06-15 20:32:00 -0300

    refactor: change content component styling

--- src/frontend/src/pages/Home/index.jsx
+++ src/frontend/src/pages/Home/index.jsx
@@ -4,12 +4,12 @@ import styles from './styles.module.scss';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.container__title}>Home</h1>
-
-      <Content 
-        title={"teste"}
-        content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisi ac ultricies fermentum, nunc nunc ullamcorper purus, nec cursus nunc nunc nec"}
-      />
+      <section className={styles.container__section}>
+        <Content 
+          title={"teste do componente de conteúdo"}
+          content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisi ac ultricies fermentum, nunc nunc ullamcorper purus, nec cursus nunc nunc nec"}
+        />
+      </section>
 
     </div>
   );

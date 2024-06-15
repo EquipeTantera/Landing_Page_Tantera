4d4e899eb3520032e75f90b989bdfd9536d1aca2
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-06-15 20:34:32 -0300

    refactor: Add example of using the content component on the home page

--- src/frontend/src/pages/Home/index.jsx
+++ src/frontend/src/pages/Home/index.jsx
@@ -9,6 +9,10 @@ export default function Home() {
           title={"teste do componente de conteúdo"}
           content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisi ac ultricies fermentum, nunc nunc ullamcorper purus, nec cursus nunc nunc nec"}
         />
+        
+        <Content 
+          content={"Componente de conteúdo sem subtitulo"}
+        />
       </section>
 
     </div>

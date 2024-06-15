64510d43e5e1b508fb0508e92bcff3657a7acb9c
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-06-15 20:28:27 -0300

    feat: add content component and desktop and mobile

--- src/frontend/src/pages/Home/index.jsx
+++ src/frontend/src/pages/Home/index.jsx
@@ -1,13 +1,15 @@
+import Content from '../../components/Content';
 import styles from './styles.module.scss';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1 className={styles.container__title}>Home</h1>
-      <h1 className={styles.container__title}>Home</h1>
-      <h1 className={styles.container__title}>Home</h1>
-      <h1 className={styles.container__title}>Home</h1>
-      <h1 className={styles.container__title}>Home</h1>
+
+      <Content 
+        title={"teste"}
+        content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisi ac ultricies fermentum, nunc nunc ullamcorper purus, nec cursus nunc nunc nec"}
+      />
 
     </div>
   );

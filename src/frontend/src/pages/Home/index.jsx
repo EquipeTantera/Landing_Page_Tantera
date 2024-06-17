278d09ecac08e388790c6aa464dcbf9e69e73184
Biabborges <bianca.lins@sou.inteli.edu.br>
2024-06-17 10:48:16 -0300

    fix: correcting the 'home' structure

--- src/frontend/src/pages/Home/index.jsx
+++ src/frontend/src/pages/Home/index.jsx
@@ -5,23 +5,19 @@ import styles from './styles.module.scss';
 export default function Home() {
   return (
     <>
-      <HorizontalSubtitle title='teste teste' colorImage='red' tag={false}/>
+      <HorizontalSubtitle title='teste teste' colorImage='red' tag={false} />
       <div className={styles.container}>
-        <h1 className={styles.container__title}>Home</h1>
+        <section className={styles.container__section}>
+          <Content 
+            title={"teste do componente de conteúdo"}
+            content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisi ac ultricies fermentum, nunc nunc ullamcorper purus, nec cursus nunc nunc nec"}
+          />
+          
+          <Content 
+            content={"Componente de conteúdo sem subtitulo"}
+          />
+        </section>
       </div>
     </>
-    <div className={styles.container}>
-      <section className={styles.container__section}>
-        <Content 
-          title={"teste do componente de conteúdo"}
-          content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisi ac ultricies fermentum, nunc nunc ullamcorper purus, nec cursus nunc nunc nec"}
-        />
-        
-        <Content 
-          content={"Componente de conteúdo sem subtitulo"}
-        />
-      </section>
-
-    </div>
   );
 }

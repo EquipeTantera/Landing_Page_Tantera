d082a94bee0567d2972309a5baeee8bc82f38e4c
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-08-05 19:13:36 -0300

    refactor: remove call from main title component

--- src/frontend/src/pages/Home/index.jsx
+++ src/frontend/src/pages/Home/index.jsx
@@ -1,17 +1,11 @@
 import HorizontalSubtitle from '../../components/HorizontalSubtitle';
 import Content from '../../components/Content';
-import MainTitle from '../../components/MainTitle';
 import styles from './styles.module.scss';
 import Footer from '../../components/Footer';
 
 export default function Home() {
   return (
     <>
-    <MainTitle
-    shadowText="Eventos"
-    mainText="Eventos"
-    bgColor={styles.container__section}
-    />
       <HorizontalSubtitle title='teste teste' colorImage='red' tag={false} />
       <div className={styles.container}>
         <section className={styles.container__section}>

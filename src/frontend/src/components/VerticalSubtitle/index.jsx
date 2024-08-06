b13fda5198f08a1b6b05efcbf40a128eea3d906e
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-08-06 12:43:26 -0300

    feat: add footer to standard page component

---
+++ src/frontend/src/components/VerticalSubtitle/index.jsx
@@ -0,0 +1,7 @@
+export default function VerticalSubtitle() {
+  return (
+    <div className="vertical-subtitle">
+      <h2 className="vertical-subtitle__title">teste teste</h2>
+    </div>
+  );
+}
--- src/frontend/src/pages/Home/index.jsx
+++ src/frontend/src/pages/Home/index.jsx
@@ -1,24 +1,14 @@
-import HorizontalSubtitle from '../../components/HorizontalSubtitle';
-import Content from '../../components/Content';
+import VerticalSubtitle from '../../components/VerticalSubtitle';
 import styles from './styles.module.scss';
-import Footer from '../../components/Footer';
 
 export default function Home() {
   return (
     <>
-      <HorizontalSubtitle title='teste teste' colorImage='red' tag={false} />
       <div className={styles.container}>
         <section className={styles.container__section}>
-          <Content 
-            title={"teste do componente de conteúdo"}
-            content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisi ac ultricies fermentum, nunc nunc ullamcorper purus, nec cursus nunc nunc nec"}
-          />
-          <Content 
-            content={"Componente de conteúdo sem subtitulo"}
-          />
+          <VerticalSubtitle />
         </section>
       </div>
-      <Footer />
     </>
   );
 }

6f90ef496a610dc953f20537a401584cc392533c
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-08-06 12:45:36 -0300

    refactor: change font size in main title component

--- src/frontend/src/pages/Parceiros/index.jsx
+++ src/frontend/src/pages/Parceiros/index.jsx
@@ -1,18 +1,14 @@
 import HorizontalSubtitle from '../../components/HorizontalSubtitle';
 import MainTitle from '../../components/MainTitle';
-import styles from './styles.module.scss';
 
 export default function Parceiros() {
   return (
     <>
-    <MainTitle
-    shadowText="Parceiros"
-    mainText="Parceiros"
-    bgColor={styles.container__section}
-    />
+      <MainTitle
+        shadowText="Parceiros"
+        mainText="Parceiros"
+      />
       <HorizontalSubtitle title='teste teste' colorImage='red' tag={false} />
-      <div className={styles.container}>
-      </div>
     </>
   );
 }

065a5cc3260260a6f07ec6661af20cd6501f9935
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-08-14 17:49:20 -0300

    feat: add large event card

--- src/frontend/src/pages/Home/index.jsx
+++ src/frontend/src/pages/Home/index.jsx
@@ -1,24 +1,20 @@
 import styles from './styles.module.scss';
-import LargePartnerCard from '../../components/LargePartnerCard';
+import LargeEventCard from '../../components/LargeEventCard';
 
 export default function Home() {
   return (
     <>
       <div className={styles.container}>
         <section className={styles.container__section}>
-          <LargePartnerCard 
+          <LargeEventCard 
             name="Furioso"
             description="A Furioso é uma empresa de tecnologia que atua no mercado de desenvolvimento de software."
             image='/partner-furioso.png'
             fullImage='/partner-furioso-full.png'
-            events={[
-              { name: 'Evento 1', date: '01/01/2021' },
-              { name: 'Evento 2', date: '02/02/2021' },
-            ]}
-            impacts={[
-              { name: 'Impacto 1' },
-              { name: 'Impacto 2' },
-              { name: 'Impacto 2' },
+            address={"Rua das Flores, 123"}
+            dates={[
+              { date: '20/01', startHour: '10h', endHour: '11h' },
+              { date: '20/01', startHour: '10h', endHour: '11h' },
             ]}
             textButton="Conheça mais"
             linkButton="/furioso"

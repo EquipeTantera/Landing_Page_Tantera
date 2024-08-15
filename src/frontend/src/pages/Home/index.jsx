a22668c1491bbb4cff34b408884ae86205866633
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-08-14 22:24:29 -0300

    feat: add large product card

--- src/frontend/src/pages/Home/index.jsx
+++ src/frontend/src/pages/Home/index.jsx
@@ -1,23 +1,20 @@
 import styles from './styles.module.scss';
-import LargeEventCard from '../../components/LargeEventCard';
+import LargeProductCard from '../../components/LargeProductCard';
 
 export default function Home() {
   return (
     <>
       <div className={styles.container}>
         <section className={styles.container__section}>
-          <LargeEventCard 
-            name="Furioso"
-            description="A Furioso é uma empresa de tecnologia que atua no mercado de desenvolvimento de software."
-            image='/partner-furioso.png'
-            fullImage='/partner-furioso-full.png'
-            address={"Rua das Flores, 123"}
-            dates={[
-              { date: '20/01', startHour: '10h', endHour: '11h' },
-              { date: '20/01', startHour: '10h', endHour: '11h' },
-            ]}
-            textButton="Conheça mais"
-            linkButton="/furioso"
+          <LargeProductCard 
+            name="Camiseta Furioso"
+            description="Camiseta da empresa Furioso"
+            fullImage="/partner-furioso-full.png"
+            sizes={['P', 'M', 'G', 'GG']}
+            colors={['Branca', 'Preta', 'Vermelha']}
+            price="50,00"
+            textButton="Comprar"
+            linkButton="/comprar"
           />
         </section>
       </div>

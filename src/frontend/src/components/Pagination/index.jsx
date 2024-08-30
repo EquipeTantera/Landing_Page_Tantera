ca279fb5a3cf506b637ba5cd81334750da2bd6bc
Biabborges <bianca.lins@sou.inteli.edu.br>
2024-08-30 13:10:18 -0300

    feat: add responsive component

--- src/frontend/src/components/Pagination/index.jsx
+++ src/frontend/src/components/Pagination/index.jsx
@@ -6,37 +6,41 @@ export default function Pagination({ totalPages, currentPage, onPageChange }) {
 
   return (
     <div className={styles.pagination}>
-      {pages.map(page => (
-        <span
-          key={page}
-          className={`${styles.pagination__page} ${page === currentPage ? styles['pagination__page--active'] : ''}`}
-          onClick={() => onPageChange(page)}
+      <div className={styles.pagination__numbers}>
+        {pages.map(page => (
+          <span
+            key={page}
+            className={`${styles.pagination__page} ${page === currentPage ? styles['pagination__page--active'] : ''}`}
+            onClick={() => onPageChange(page)}
+          >
+            {page}
+          </span>
+        ))}
+      </div>
+      <span className={styles.pagination__separator1}>|</span>
+      <div className={styles.pagination__links}>
+        <a
+          href="#"
+          className={styles.pagination__link}
+          onClick={(e) => {
+            e.preventDefault();
+            onPageChange(currentPage + 1);
+          }}
         >
-          {page}
-        </span>
-      ))}
-      <span className={styles.pagination__separator}>|</span>
-      <a
-        href="#"
-        className={styles.pagination__link}
-        onClick={(e) => {
-          e.preventDefault();
-          onPageChange(currentPage + 1);
-        }}
-      >
-        Próxima página
-      </a>
-      <span className={styles.pagination__separator}>|</span>
-      <a
-        href="#"
-        className={styles.pagination__link}
-        onClick={(e) => {
-          e.preventDefault();
-          onPageChange(totalPages);
-        }}
-      >
-        Última página
-      </a>
+          Próxima página
+        </a>
+        <span className={styles.pagination__separator}>|</span>
+        <a
+          href="#"
+          className={styles.pagination__link}
+          onClick={(e) => {
+            e.preventDefault();
+            onPageChange(totalPages);
+          }}
+        >
+          Última página
+        </a>
+      </div>
     </div>
   );
 }

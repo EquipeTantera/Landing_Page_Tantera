e173aeb4a55ebb8cbced25526d45a2aa0494eea5
Biabborges <bianca.lins@sou.inteli.edu.br>
2024-08-29 22:44:21 -0300

    feat: adds new prop to component input

--- src/frontend/src/components/Input/index.jsx
+++ src/frontend/src/components/Input/index.jsx
@@ -1,39 +1,40 @@
 import PropTypes from 'prop-types';
 import styles from './styles.module.scss';
 
-export default function Input({ type, placeholder, options }) {
-  if (type === 'select') {
-    return (
-      <div className={styles.inputContainer}>
-        <select className={styles.input} defaultValue="">
-          <option value="" disabled>{placeholder}</option>
-          {options.map((option, index) => (
-            <option key={index} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
-        <div className={styles.icon}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="34"
-            height="16"
-            viewBox="0 0 34 16"
-            fill="none"
-          >
-            <path d="M1 1L17 14.5L33 1" stroke="black" strokeWidth="2" />
-          </svg>
-        </div>
-      </div>
-    );
-  }
-
+export default function Input({ type, placeholder, options, label }) {
   return (
-    <input
-      type={type}
-      className={styles.input}
-      placeholder={placeholder}
-    />
+    <div className={styles.inputWrapper}>
+      {label && <label className={styles.label}>{label}</label>}
+      {type === 'select' ? (
+        <div className={styles.inputContainer}>
+          <select className={styles.input} defaultValue="">
+            <option value="" disabled>{placeholder}</option>
+            {options.map((option, index) => (
+              <option key={index} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+          <div className={styles.icon}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="34"
+              height="16"
+              viewBox="0 0 34 16"
+              fill="none"
+            >
+              <path d="M1 1L17 14.5L33 1" stroke="black" strokeWidth="2" />
+            </svg>
+          </div>
+        </div>
+      ) : (
+        <input
+          type={type}
+          className={styles.input}
+          placeholder={placeholder}
+        />
+      )}
+    </div>
   );
 }
 
@@ -43,12 +44,14 @@ Input.propTypes = {
   options: PropTypes.arrayOf(
     PropTypes.shape({
       value: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired
+      label: PropTypes.string.isRequired,
     })
-  )
+  ),
+  label: PropTypes.string,
 };
 
 Input.defaultProps = {
   placeholder: '',
-  options: []
+  options: [],
+  label: '', 
 };

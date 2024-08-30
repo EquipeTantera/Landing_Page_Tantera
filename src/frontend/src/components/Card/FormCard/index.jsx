c6c771409d196535dd640a4368a0c5e99d55881c
Biabborges <bianca.lins@sou.inteli.edu.br>
2024-08-29 22:45:23 -0300

    feat: add component formCard

---
+++ src/frontend/src/components/Card/FormCard/index.jsx
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import Input from '../../Input';
+import Button from '../../Button';
+import styles from './styles.module.scss';
+
+export default function FormCard({ title, inputs, textButton, linkButton }) {
+  return (
+    <div className={styles.formCard}>
+      <h2 className={styles.formCard__title}>{title}</h2>
+      <div className={styles.formCard__content}>
+        {inputs.map((inputProps, index) => (
+          <Input key={index} {...inputProps} />
+        ))}
+      </div>
+      {textButton && linkButton && (
+        <div className={styles.formCard__button}>
+          <Button title={textButton} path={linkButton} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+FormCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  inputs: PropTypes.arrayOf(
+    PropTypes.shape({
+      type: PropTypes.oneOf(['text', 'select']).isRequired,
+      placeholder: PropTypes.string,
+      options: PropTypes.arrayOf(
+        PropTypes.shape({
+          value: PropTypes.string.isRequired,
+          label: PropTypes.string.isRequired,
+        })
+      ),
+    })
+  ).isRequired,
+  textButton: PropTypes.string,
+  linkButton: PropTypes.string, 
+};
+
+FormCard.defaultProps = {
+  textButton: null,
+  linkButton: null,
+};

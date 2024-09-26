42f0ee5a4dd47434eab632c08e1c0047fde6e4bf
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-09-25 22:11:25 -0300

    refactor: changes general styling of the events page

--- src/frontend/src/pages/Events/index.jsx
+++ src/frontend/src/pages/Events/index.jsx
@@ -38,60 +38,62 @@ export default function Events() {
   return (
     <>
       <MainTitle shadowText="Eventos" mainText="Eventos" />
-      <HorizontalSubtitle title="Festas" colorImage="purple" titleSize="3rem" />
-      <div className={styles.nextParty}>
-        <p className={styles.nextPartyText}>próxima festa</p>
-      </div>
+
+      <section className={styles.container__fests}>
+        <HorizontalSubtitle title="Festas" colorImage="purple" titleSize="3rem" />
+        <div className={styles.container__fests__nextParty}>
+          <p className={styles.container__fests__nextParty__text}>Próxima festa</p>
+        </div>
+        
+        <div className={styles.container__fests__cards}>
+          <MediumEventCard 
+            title="O Covil"
+            description="Nova festa da Tan Tan"
+            image="/partner-furioso-full.png"
+            address="Rua Furiosa, 123"
+            date="10/09/2023"
+            ticket="01/01/01"
+            buttonText="Saiba mais"
+            linkButton="/comprar"
+          />
+        </div>
+
+        <div className={styles.container__fests__nextParty}>
+          <p className={styles["container__fests__nextParty__text--second"]}>Festas anteriores</p>
+        </div>
+
+        <div className={styles.container__fests__carousel}>
+          <CarouselSmallEventCard events={pastEvents} interval={3000} />
+        </div>
+      </section>
+
+      <section className={styles.container__championships}>  
+        <HorizontalSubtitle title="Campeonatos" colorImage="red" titleSize="3rem" />
+        <div className={styles.container__championships__nextParty}>
+          <p className={styles.container__fests__nextParty__text}>Próximo campeonato</p>
+        </div>
       
-      <div className={styles.eventCardContainer}>
-        <MediumEventCard 
-          title="O Covil"
-          description="Nova festa da Tan Tan"
-          image="/partner-furioso-full.png"
-          address="Rua Furiosa, 123"
-          date="10/09/2023"
-          ticket="01/01/01"
-          buttonText="Saiba mais"
-          linkButton="/comprar"
-        />
-      </div>
+        <div className={styles.container__championships__cards}>
+          <MediumEventCard 
+            title="Campeonato de basquete"
+            description="Novo campeonato de basquete"
+            image="/partner-furioso-full.png"
+            address="Rua Furiosa, 123"
+            date="10/09/2023"
+            ticket="01/01/01"
+            buttonText="Saiba mais"
+            linkButton="/comprar"
+          />
+        </div>
 
-      <div className={styles.spacer}></div>
+        <div className={styles.container__championships__nextParty}>
+          <p className={styles["container__championships__nextParty__text--second"]}>Campeonatos Anteriores</p>
+        </div>
 
-      <div className={styles.nextParty}>
-        <p className={styles.nextPartyText}>Festas anteriores</p>
-      </div>
-
-      <div className={styles.carouselContainer}>
-        <CarouselSmallEventCard events={pastEvents} interval={3000} />
-      </div>
-      <HorizontalSubtitle title="Campeonatos" colorImage="red" titleSize="3rem" />
-      <div className={styles.nextParty}>
-        <p className={styles.nextPartyText}>Próximo campeonato</p>
-      </div>
-      
-      <div className={styles.eventCardContainer}>
-        <MediumEventCard 
-          title="Campeonato de basquete"
-          description="Novo campeonato de basquete"
-          image="/partner-furioso-full.png"
-          address="Rua Furiosa, 123"
-          date="10/09/2023"
-          ticket="01/01/01"
-          buttonText="Saiba mais"
-          linkButton="/comprar"
-        />
-      </div>
-
-      <div className={styles.spacer}></div>
-
-      <div className={styles.nextParty}>
-        <p className={styles.nextPartyText}>Campeonatos Anteriores</p>
-      </div>
-
-      <div className={styles.carouselContainer}>
-        <CarouselSmallEventCard events={pastEvents} interval={3000} />
-      </div>
+        <div className={styles.container__championships__carousel}>
+          <CarouselSmallEventCard events={pastEvents} interval={3000} />
+        </div>
+      </section>  
     </>
   );
 }

5dc53a2fac59ca109d7ead3d987f5147ec9847be
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-09-26 15:50:20 -0300

    feat: add  filtering by category

--- src/frontend/src/pages/Products/index.jsx
+++ src/frontend/src/pages/Products/index.jsx
@@ -15,7 +15,15 @@ export default function Products() {
   const [showMoreFesta, setShowMoreFesta] = useState(false);
   const [showMoreUniformes, setShowMoreUniformes] = useState(false);
   const [showMoreColecoes, setShowMoreColecoes] = useState(false);
+  const [filters, setFilters] = useState({
+    sort: '',
+    categories: [],
+    sizes: [],
+    colors: [],
+    gender: '',
+  });
 
+  // Carregar dados dos produtos
   useEffect(() => {
     const fetchProductsData = async () => {
       try {
@@ -23,54 +31,36 @@ export default function Products() {
         const products = response.data.data;
 
         const formattedProducts = products.map((product) => {
-          const imageUrl = product?.attributes?.image?.data?.[0]?.attributes
-            ?.url
-            ? product.attributes.image.data[0].attributes.url
-            : "";
+          const imageUrl = product?.attributes?.image?.data?.[0]?.attributes?.url || "";
 
           const colors = Array.isArray(product?.attributes?.colors_id?.data)
-            ? product.attributes.colors_id.data.map(
-                (color) => color.attributes.name
-              )
-            : [];
+          ? product.attributes.colors_id.data.map((color) => color.attributes.name)
+          : [];
 
           const sizes = Array.isArray(product?.attributes?.sizes_id?.data)
-            ? product.attributes.sizes_id.data.map(
-                (size) => size.attributes.name
-              )
-            : [];
+          ? product.attributes.sizes_id.data.map((size) => size.attributes.name)
+          : [];
 
           const genres = Array.isArray(product?.attributes?.genres_id?.data)
-            ? product.attributes.genres_id.data.map(
-                (genre) => genre.attributes.name
-              )
-            : [];
+          ? product.attributes.genres_id.data.map((genre) => genre.attributes.name)
+          : [];
 
-          const categories = Array.isArray(
-            product?.attributes?.categories_ids?.data
-          )
-            ? product.attributes.categories_ids.data.map(
-                (category) => category.attributes.category
-              )
+          const categories = Array.isArray(product?.attributes?.categories_ids?.data)
+            ? product.attributes.categories_ids.data.map((category) => category.attributes.category)
             : [];
 
           const sold_out = product?.attributes?.sold_out || false;
 
-          // Formatação dos dados do produto
           return {
             title: product?.attributes?.title || "",
-            price: product?.attributes?.price
-              ? product.attributes.price.toFixed(2)
-              : "0.00",
-            image: imageUrl.startsWith("http")
-              ? imageUrl
-              : `${BASE_URL}${imageUrl}`,
+            price: product?.attributes?.price ? product.attributes.price.toFixed(2) : "0.00",
+            image: imageUrl.startsWith("http") ? imageUrl : `${BASE_URL}${imageUrl}`,
             description: product?.attributes?.description || "",
             sold_out,
             colors,
             sizes,
             genres,
-            categories, // As categorias do produto
+            categories,
             buttonText: "Ver Produto",
             buttonPath: `/produto/${product.id}`,
           };
@@ -93,6 +83,56 @@ export default function Products() {
     setModalOpen(false);
   };
 
+  const handleApplyFilters = (appliedFilters) => {
+    setFilters(appliedFilters);
+  };
+
+  // Função para filtrar os produtos
+  const getFilteredProducts = () => {
+    let filteredProducts = productsData;
+  
+    // Filtrar por categorias
+    if (filters.categories.length > 0) {
+      filteredProducts = filteredProducts.filter((product) =>
+        product.categories.some((category) => filters.categories.includes(category))
+      );
+    }
+  
+    // Filtrar por tamanhos
+    if (filters.sizes.length > 0) {
+      filteredProducts = filteredProducts.filter((product) =>
+        product.sizes.some((size) => filters.sizes.includes(size))
+      );
+    }
+  
+    // Filtrar por cores
+    if (filters.colors.length > 0) {
+      filteredProducts = filteredProducts.filter((product) =>
+        product.colors.some((color) => filters.colors.includes(color))
+      );
+    }
+  
+    // Filtrar por gênero
+    if (filters.gender) {
+      filteredProducts = filteredProducts.filter((product) =>
+        product.genres.includes(filters.gender)
+      );
+    }
+  
+    // Ordenar por preço
+    if (filters.sort === 'menor') {
+      filteredProducts = filteredProducts.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+    } else if (filters.sort === 'maior') {
+      filteredProducts = filteredProducts.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
+    }
+  
+    return filteredProducts;
+  };
+  
+
+  const filteredProducts = getFilteredProducts();
+  const hasFiltersApplied = filters.categories.length > 0 || filters.sizes.length > 0 || filters.colors.length > 0 || filters.gender;
+
   // Função para filtrar produtos pela categoria
   const filterProductsByCategory = (categoryName) => {
     return productsData.filter((product) =>
@@ -104,22 +144,17 @@ export default function Products() {
     <>
       <MainTitle shadowText="Produtos" mainText="Produtos" />
       <PaperBackground>
-        {/* Seção de Acessórios de Festa */}
-        <section className={styles.section}>
-          <div className={styles["container-header"]}>
-            <HorizontalSubtitle
-              title="Acessórios de Festa"
-              colorImage="transparent"
-              tag={false}
-            />
-            <FilterButton text="Filtrar" onClick={handleOpenModal} />
-            <FilterModal isOpen={isModalOpen} onClose={handleCloseModal} />
-          </div>
+        {hasFiltersApplied ? (
+          <section className={styles.section}>
+            <div className={styles["container-header"]}>
+              <HorizontalSubtitle title="Produtos Filtrados" colorImage="transparent" tag={false} />
+              <FilterButton text="Filtrar" onClick={handleOpenModal} />
+              <FilterModal isOpen={isModalOpen} onClose={handleCloseModal} onApplyFilters={handleApplyFilters} />
+            </div>
 
-          <div className={styles["container-products"]}>
-            {filterProductsByCategory("Acessórios de Festa").length > 0 &&
-              filterProductsByCategory("Acessórios de Festa").map(
-                (product, index) => (
+            <div className={styles["container-products"]}>
+              {filteredProducts.length > 0 ? (
+                filteredProducts.map((product, index) => (
                   <SmallProductCard
                     key={index}
                     title={product.title}
@@ -130,126 +165,130 @@ export default function Products() {
                     buttonPath={product.buttonPath}
                     isAvailable={!product.sold_out}
                   />
-                )
+                ))
+              ) : (
+                <p>Nenhum produto encontrado com os filtros aplicados.</p>
               )}
-          </div>
+            </div>
+          </section>
+        ) : (
+          <>
+            <section className={styles.section}>
+              <div className={styles["container-header"]}>
+                <HorizontalSubtitle title="Acessórios de Festa" colorImage="transparent" tag={false} />
+                <FilterButton text="Filtrar" onClick={handleOpenModal} />
+                <FilterModal isOpen={isModalOpen} onClose={handleCloseModal} onApplyFilters={handleApplyFilters} />
+              </div>
 
-          <button
-            onClick={() => setShowMoreFesta((prev) => !prev)}
-            className={styles["container-products__button"]}
-          >
-            {showMoreFesta ? (
-              <p className={styles["container-products__button__paragraph"]}>
-                Ocultar produtos
-              </p>
-            ) : (
-              <p className={styles["container-products__button__paragraph"]}>
-                Ver mais produtos de{" "}
-                <span
-                  className={
-                    styles["container-products__button__paragraph__span"]
-                  }
-                >
-                  acessórios de festa
-                </span>
-              </p>
-            )}
-          </button>
-        </section>
+              <div className={styles["container-products"]}>
+                {filterProductsByCategory("Acessórios de Festa").map((product, index) => (
+                  <SmallProductCard
+                    key={index}
+                    title={product.title}
+                    price={product.price}
+                    image={product.image}
+                    description={product.description}
+                    buttonText={product.buttonText}
+                    buttonPath={product.buttonPath}
+                    isAvailable={!product.sold_out}
+                  />
+                ))}
+              </div>
 
-        {/* Seção de Uniformes */}
-        <section className={styles.section}>
-          <HorizontalSubtitle
-            title="Uniformes"
-            colorImage="transparent"
-            tag={false}
-            titleSize="2.5rem"
-          />
-          <div className={styles["container-products"]}>
-            {filterProductsByCategory("Uniformes").length > 0 &&
-              filterProductsByCategory("Uniformes").map((product, index) => (
-                <SmallProductCard
-                  key={index}
-                  title={product.title}
-                  price={product.price}
-                  image={product.image}
-                  description={product.description}
-                  buttonText={product.buttonText}
-                  buttonPath={product.buttonPath}
-                  isAvailable={!product.sold_out}
-                />
-              ))}
-          </div>
+              <button
+                onClick={() => setShowMoreFesta((prev) => !prev)}
+                className={styles["container-products__button"]}
+              >
+                {showMoreFesta ? (
+                  <p className={styles["container-products__button__paragraph"]}>
+                    Ocultar produtos
+                  </p>
+                ) : (
+                  <p className={styles["container-products__button__paragraph"]}>
+                    Ver mais produtos de{" "}
+                    <span className={styles["container-products__button__paragraph__span"]}>
+                      acessórios de festa
+                    </span>
+                  </p>
+                )}
+              </button>
+            </section>
 
-          <button
-            onClick={() => setShowMoreUniformes((prev) => !prev)}
-            className={styles["container-products__button"]}
-          >
-            {showMoreUniformes ? (
-              <p className={styles["container-products__button__paragraph"]}>
-                Ocultar produtos
-              </p>
-            ) : (
-              <p className={styles["container-products__button__paragraph"]}>
-                Ver mais produtos de{" "}
-                <span
-                  className={
-                    styles["container-products__button__paragraph__span"]
-                  }
-                >
-                  uniformes
-                </span>
-              </p>
-            )}
-          </button>
-        </section>
+            {/* Seção de Uniformes */}
+            <section className={styles.section}>
+              <HorizontalSubtitle title="Uniformes" colorImage="transparent" tag={false} titleSize="2.5rem" />
+              <div className={styles["container-products"]}>
+                {filterProductsByCategory("Uniformes").map((product, index) => (
+                  <SmallProductCard
+                    key={index}
+                    title={product.title}
+                    price={product.price}
+                    image={product.image}
+                    description={product.description}
+                    buttonText={product.buttonText}
+                    buttonPath={product.buttonPath}
+                    isAvailable={!product.sold_out}
+                  />
+                ))}
+              </div>
 
-        {/* Seção de Coleções */}
-        <section className={styles.section}>
-          <HorizontalSubtitle
-            title="Coleções"
-            colorImage="transparent"
-            tag={false}
-            titleSize="2.5rem"
-          />
-          <div className={styles["container-products"]}>
-            {filterProductsByCategory("Coleções").length > 0 &&
-              filterProductsByCategory("Coleções").map((product, index) => (
-                <SmallProductCard
-                  key={index}
-                  title={product.title}
-                  price={product.price}
-                  image={product.image}
-                  description={product.description}
-                  buttonText={product.buttonText}
-                  buttonPath={product.buttonPath}
-                  isAvailable={!product.sold_out}
-                />
-              ))}
-          </div>
+              <button
+                onClick={() => setShowMoreUniformes((prev) => !prev)}
+                className={styles["container-products__button"]}
+              >
+                {showMoreUniformes ? (
+                  <p className={styles["container-products__button__paragraph"]}>
+                    Ocultar produtos
+                  </p>
+                ) : (
+                  <p className={styles["container-products__button__paragraph"]}>
+                    Ver mais produtos de{" "}
+                    <span className={styles["container-products__button__paragraph__span"]}>
+                      uniformes
+                    </span>
+                  </p>
+                )}
+              </button>
+            </section>
 
-          <button
-            onClick={() => setShowMoreColecoes((prev) => !prev)}
-            className={styles["container-products__button"]}
-          >
-            {showMoreColecoes ? (
-              <p className={styles["container-products__button__paragraph"]}>
-                Ocultar produtos
-              </p>
-            ) : (
-              <p className={styles["container-products__button__paragraph"]}>
-                Ver mais produtos de{" "}
-                <span
-                  className={
-                    styles["container-products__button__paragraph__span"]
-                  }
-                >
-                  coleções
-                </span>
-              </p>
-            )}
-          </button>
-        </section>
+            {/* Seção de Coleções */}
+            <section className={styles.section}>
+              <HorizontalSubtitle title="Coleções" colorImage="transparent" tag={false} titleSize="2.5rem" />
+              <div className={styles["container-products"]}>
+                {filterProductsByCategory("Coleções").map((product, index) => (
+                  <SmallProductCard
+                    key={index}
+                    title={product.title}
+                    price={product.price}
+                    image={product.image}
+                    description={product.description}
+                    buttonText={product.buttonText}
+                    buttonPath={product.buttonPath}
+                    isAvailable={!product.sold_out}
+                  />
+                ))}
+              </div>
+
+              <button
+                onClick={() => setShowMoreColecoes((prev) => !prev)}
+                className={styles["container-products__button"]}
+              >
+                {showMoreColecoes ? (
+                  <p className={styles["container-products__button__paragraph"]}>
+                    Ocultar produtos
+                  </p>
+                ) : (
+                  <p className={styles["container-products__button__paragraph"]}>
+                    Ver mais produtos de{" "}
+                    <span className={styles["container-products__button__paragraph__span"]}>
+                      coleções
+                    </span>
+                  </p>
+                )}
+              </button>
+            </section>
+          </>
+        )}
       </PaperBackground>
     </>
   );

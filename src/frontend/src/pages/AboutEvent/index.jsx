ca41c8f4823d1cf4d30ce674a3ce5507e3eee750
Biabborges <bianca.lins@sou.inteli.edu.br>
2024-09-26 11:28:05 -0300

    feat: add button to event page

--- src/frontend/src/pages/AboutEvent/index.jsx
+++ src/frontend/src/pages/AboutEvent/index.jsx
@@ -2,6 +2,7 @@ import MainTitle from "../../components/MainTitle";
 import VerticalSubtitle from "../../components/VerticalSubtitle";
 import Content from "../../components/Content";
 import EventInformationCard from "../../components/Card/InformationCard/EventInformationCard"; // Importando o EventInformationCard
+import Button from "../../components/Buttons/Button";
 import styles from "./styles.module.scss";
 
 export default function AboutEvents() {
@@ -11,7 +12,7 @@ export default function AboutEvents() {
       <div className={styles.backgroundContainer}>
         <div className={styles.overlayContent}>
           <VerticalSubtitle 
-            title="Fundação"
+            title="Sobre o evento"
             imageBackground="purple"
             className={styles.verticalSubtitle}
           />
@@ -20,28 +21,31 @@ export default function AboutEvents() {
               content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec aliquet sem. Morbi volutpat neque sed auctor elementum. Donec justo magna, porttitor in sagittis id, malesuada ac est. Aenean congue metus sed mauris pretium, vitae hendrerit nunc tincidunt."
               className={styles.content}
             />
+            <div className={styles.buttonContainer}>
+              <Button title='Participar' path='/' />
+            </div>
           </div>
         </div>
       </div>
       <div className={styles.eventInfoContainer}>
-          <EventInformationCard 
-            address='Rua dos Bobos, 0'
-            dates={[
-              {
-                date: '01/01/2021',
-                startHour: '08:00',
-                endHour: '12:00',
-              },
-              {
-                date: '02/01/2021',
-                startHour: '08:00',
-                endHour: '12:00',
-              },
-            ]}
-            observation='Observação do evento'
-            image='/copa-inteli.png'
-          />
-        </div>
+        <EventInformationCard 
+          address='Rua dos Bobos, 0'
+          dates={[
+            {
+              date: '01/01/2021',
+              startHour: '08:00',
+              endHour: '12:00',
+            },
+            {
+              date: '02/01/2021',
+              startHour: '08:00',
+              endHour: '12:00',
+            },
+          ]}
+          observation='Observação do evento'
+          image='/copa-inteli.png'
+        />
+      </div>
     </>
   );
 }

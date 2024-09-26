2ca5a517ff0577e8762880e765bdd4e31ab9ecb5
Biabborges <bianca.lins@sou.inteli.edu.br>
2024-09-26 00:02:03 -0300

    feat: add form to about us

--- src/frontend/src/pages/About-us/index.jsx
+++ src/frontend/src/pages/About-us/index.jsx
@@ -1,11 +1,22 @@
 import MainTitle from "../../components/MainTitle";
 import VerticalSubtitle from "../../components/VerticalSubtitle";
 import FullBoardCard from "../../components/Card/BoardCard/FullBoardCard";
-import styles from "./styles.module.scss";
 import HorizontalSubtitle from "../../components/HorizontalSubtitle";
 import Content from "../../components/Content";
+import FormCard from "../../components/Card/FormCard";
+import styles from "./styles.module.scss";
 
 export default function AboutUs() {
+
+  const inputs = [
+    { type: 'text', placeholder: 'Seu nome', label: 'Nome' },
+    { type: 'text', placeholder: 'Seu email', label: 'Email' },
+    { type: 'select', placeholder: 'Selecione uma opção', label: 'Selecione', options: [
+      { value: 'opcao1', label: 'Opção 1' },
+      { value: 'opcao2', label: 'Opção 2' },
+    ] 
+    },
+  ];
   return (
     <>
       <MainTitle 
@@ -18,14 +29,13 @@ export default function AboutUs() {
           imageBackground="purple"
           className={styles.verticalSubtitle}
         />
-      <div style={{ width: '600px', margin: '0 auto' }}>
-        <Content 
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec aliquet sem. Morbi volutpat neque sed auctor elementum. Donec justo magna, porttitor in sagittis id, malesuada ac est. Aenean congue metus sed mauris pretium, vitae hendrerit nunc tincidunt."
-          className={styles.content}
-        />
+        <div style={{ width: '600px', margin: '0 auto' }}>
+          <Content 
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec aliquet sem. Morbi volutpat neque sed auctor elementum. Donec justo magna, porttitor in sagittis id, malesuada ac est. Aenean congue metus sed mauris pretium, vitae hendrerit nunc tincidunt."
+            className={styles.content}
+          />
         </div>
       </div>
-
       <div className={styles.imageContainer}>
         <img 
           src="/background_purple_events.png" 
@@ -33,14 +43,12 @@ export default function AboutUs() {
           className={styles.aboutUsImage}
         />
       </div>
-      
       <div className={styles.directorSection}>
         <VerticalSubtitle 
           title="Diretoria"
           imageBackground="red"
           subtitle="presidência"
         />
-        
         <div className={styles.fullBoardCardContainer}>
           {[...Array(6)].map((_, index) => (
             <FullBoardCard 
@@ -53,9 +61,32 @@ export default function AboutUs() {
         </div>
       </div>
       <HorizontalSubtitle 
-            title="Mascote"
-            colorImage="purple"
+        title="Mascote"
+        colorImage="purple"
+      />
+      <div className={styles.mascotContainer}>
+        <img 
+          src="/mascot.png" 
+          alt="Mascote"
+          className={styles.mascotImage}
+        />
+      </div>
+
+      <div className={styles.centeredContentContainer}>
+        <Content 
+          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec aliquet sem. Morbi volutpat neque sed auctor elementum. Donec justo magna, porttitor in sagittis id, malesuada ac est. Aenean congue metus sed mauris pretium, vitae hendrerit nunc tincidunt.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec aliquet sem. Morbi volutpat neque sed auctor elementum. Donec justo magna, porttitor in sagittis id, malesuada ac est. Aenean congue metus sed mauris pretium, vitae hendrerit nunc tincidunt."
+          className={styles.centeredContent}
+        />
+      </div>
+      <div className={styles.centeredContentForm}>
+      <FormCard 
+            title="Entre em Contato" 
+            inputs={inputs} 
+            textButton="Enviar"
+            linkButton="/submit"
           />
+      </div>
     </>
   );
 }

e74e55cb97748764021f8a86d5d9940d211bee70
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-09-26 09:38:41 -0300

    refactor: add sould out in small product card

--- src/frontend/src/components/Card/SmallCard/SmallProductCard/index.jsx
+++ src/frontend/src/components/Card/SmallCard/SmallProductCard/index.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import styles from './styles.module.scss';
 import Button from '../../../Buttons/Button';
 
-export default function SmallProductCard({ title, description, price, image, buttonText, buttonPath }) {
+export default function SmallProductCard({ title, description, price, image, buttonText, buttonPath, isAvailable }) {
   return (
     <div className={styles.container}>
       <div className={styles.container__infos}>
@@ -11,9 +11,12 @@ export default function SmallProductCard({ title, description, price, image, but
           <img src={image} alt={title} className={styles.container__infos__div__image} />
         </div>
         <>
-          <div className={styles["container__infos__product-price"]}>
+          <div className={`${styles["container__infos__product-price"]}  ${!isAvailable ? styles["container__infos__product-price--unavailable"] : '' }`}>
             <p className={styles["container__infos__product-price__price"]}>
-              <span className={styles["container__infos__product-price__price__span"]}>R$ </span>{price}
+              <span className={styles["container__infos__product-price__price__span"]}>
+                {isAvailable ? 'R$ ' : ''}
+              </span> 
+              {isAvailable ? price : 'Esgotado'}
             </p>
           </div>
           <p className={styles.container__infos__description}>{description}</p>
@@ -32,11 +35,10 @@ export default function SmallProductCard({ title, description, price, image, but
 
 SmallProductCard.propTypes = {
   title: PropTypes.string.isRequired,
-  date: PropTypes.string,
-  ticket: PropTypes.string,
-  description: PropTypes.string,
-  price: PropTypes.string,
+  description: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
-  buttonText: PropTypes.string,
-  buttonPath: PropTypes.string,
+  buttonText: PropTypes.string.isRequired,
+  buttonPath: PropTypes.string.isRequired,
+  isAvailable: PropTypes.bool.isRequired
 };

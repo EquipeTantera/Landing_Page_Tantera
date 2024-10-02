b48713db688cf810e542863864626890699c3ebc
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-10-02 15:43:51 -0300

    feat: add faq accordion to membership plans screen and change title colors of this component

--- src/frontend/src/pages/MembershipPlan/index.jsx
+++ src/frontend/src/pages/MembershipPlan/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import MainTitle from "../../components/MainTitle";
 import PaperBackground from "../../components/PaperBackground";
 import LargeMembershipPlanCard from "../../components/Card/LargeCard/LargeMembershipPlanCard";
+import Accordion from "../../components/Accordion";
 
 export default function MembershipPlan() {
   return (
@@ -12,23 +13,46 @@ export default function MembershipPlan() {
       />
 
       <PaperBackground>
-        {/* Componente de cartão grande de plano de sócio */}
-        <LargeMembershipPlanCard 
-          name="Plano de Sócio"
-          description="Descrição do plano de sócio"
-          fullImage="https://via.placeholder.com/150"
-          advantages={[
-            "Vantagem 1",
-            "Vantagem 2",
-            "Vantagem 3",
-          ]}
-          paymentTypes={[
-            "Tipo de pagamento 1",
-            "Tipo de pagamento 2",
-            "Tipo de pagamento 3",
-          ]}
-          price="100,00"
-        />
+        <section>
+          <LargeMembershipPlanCard 
+            name="Plano de Sócio"
+            description="Descrição do plano de sócio"
+            fullImage="https://via.placeholder.com/150"
+            advantages={[
+              "Vantagem 1",
+              "Vantagem 2",
+              "Vantagem 3",
+            ]}
+            paymentTypes={[
+              "Tipo de pagamento 1",
+              "Tipo de pagamento 2",
+              "Tipo de pagamento 3",
+            ]}
+            price="100,00"
+          />
+        </section>
+
+        <section>
+          <Accordion 
+            items={[
+              {
+                buttonText: "Pergunta 1",
+                panelText: "Resposta 1",
+                colorImage: "purple",
+              },
+              {
+                buttonText: "Pergunta 2",
+                panelText: "Resposta 2",
+                colorImage: "red",
+              },
+              {
+                buttonText: "Pergunta 3",
+                panelText: "Resposta 3",
+                colorImage: "purple",
+              },
+            ]}
+          />
+        </section>
       </PaperBackground>
     </>
   );

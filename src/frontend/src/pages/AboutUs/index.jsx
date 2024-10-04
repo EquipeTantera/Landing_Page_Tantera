477735dc52b1ceadf30f8107713bc5b1ab324586
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-10-03 21:04:08 -0300

    refactor: refactor styling of about us page

--- src/frontend/src/pages/AboutUs/index.jsx
+++ src/frontend/src/pages/AboutUs/index.jsx
@@ -23,100 +23,112 @@ export default function AboutUs() {
         shadowText="Sobre nós"
         mainText="Sobre nós"
       />
-      <div className={styles.aboutUsContainer}>
-      <HorizontalSubtitle
-          title="Fundação"
-          colorImage="purple"
+      <section className={styles.section}>
+        <div className={styles["container-about-us"]}>
+          {window.innerWidth > 768 ? (
+            <VerticalSubtitle 
+              title="Fundação"
+              imageBackground="purple"
+            />
+          ) : (
+            <HorizontalSubtitle
+              title="Fundação"
+              colorImage="purple"
+              titleSize="3rem"
+              className={styles.horizontalSubtitle}
+            />
+          )}
+
+          <div className={styles["container-about-us__content"]}>
+            <Content 
+              content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec aliquet sem. Morbi volutpat neque sed auctor elementum. Donec justo magna, porttitor in sagittis id, malesuada ac est. Aenean congue metus sed mauris pretium, vitae hendrerit nunc tincidunt."
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className={styles["section-purpose"]}>
+        <div className={styles["container-purpose"]}>
+          <div className={styles["container-purpose__div"]}>
+            <div className={styles["container-purpose__div__div-text"]}>
+              
+              <div className={styles["container-purpose__div__div-text__title"]}>
+                <h3 className={styles["container-purpose__div__div-text__title"]}>Propósito</h3>
+              </div>
+              
+              <div>
+                <p className={styles["container-purpose__div__div-text__paragraph"]}>
+                  Estabelecer <span className={styles["container-purpose__div__div-text__paragraph__span"]}>identidade</span> e <span className={styles["container-purpose__div__div-text__paragraph__span"]}>tradição</span>;
+                </p>
+                <p className={styles["container-purpose__div__div-text__paragraph"]}>
+                  Cultivar valores de <span className={styles["container-purpose__div__div-text__paragraph__span"]}>união</span> e <span className={styles["container-purpose__div__div-text__paragraph__span"]}>colaboração</span>;
+                </p>
+                <p className={styles["container-purpose__div__div-text__paragraph"]}>
+                  Fomentar o <span className={styles["container-purpose__div__div-text__paragraph__span"]}>espírito</span> <span className={styles["container-purpose__div__div-text__paragraph__span"]}>esportivo</span>;
+                </p>
+                <p className={styles["container-purpose__div__div-text__paragraph"]}>
+                  Incentivar a <span className={styles["container-purpose__div__div-text__paragraph__span"]}>vivência</span> <span className={styles["container-purpose__div__div-text__paragraph__span"]}>universitária</span>;
+                </p>
+                <p className={styles["container-purpose__div__div-text__paragraph"]}>
+                  Gerar aprendizado sobre <span className={styles["container-purpose__div__div-text__paragraph__span"]}>gestão</span> e <span className={styles["container-purpose__div__div-text__paragraph__span"]}>trabalho</span> em equipe.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.section}>
+        <HorizontalSubtitle
+            title="Diretorias"
+            colorImage="red"
+            titleSize="3rem"
+            className={styles.horizontalSubtitle}
+          />
+        <div className={styles["container-board"]}>
+          <VerticalSubtitle 
+            title="Diretoria"
+            imageBackground="red"
+            subtitle="presidência"
+          />
+          <div className={styles["container-board__div"]}>
+            {[...Array(6)].map((_, index) => (
+              <FullBoardCard 
+                key={index} 
+                nameBoard={`Nome ${index + 1}`} 
+                description="Integer ultrices elementum mauris nec tincidunt. Mauris et lectus vel nulla condimentum dapibus. Praesent " 
+                linkButton={`/diretoria/${index + 1}`}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.section}>
+        <HorizontalSubtitle 
+          title="Mascote"
           titleSize="3rem"
-          className={styles.horizontalSubtitle}
+          colorImage="purple"
         />
-        <VerticalSubtitle 
-          title="Fundação"
-          imageBackground="purple"
-          className={styles.verticalSubtitle}
-        />
-        <div className={styles.content}>
-          <Content 
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec aliquet sem. Morbi volutpat neque sed auctor elementum. Donec justo magna, porttitor in sagittis id, malesuada ac est. Aenean congue metus sed mauris pretium, vitae hendrerit nunc tincidunt."
+        <div className={styles["container-mascot"]}>
+          <img 
+            src="/mascot.png" 
+            alt="Mascote"
+            className={styles["container-mascot__image"]}
           />
         </div>
-      </div>
-      <div className={styles.imageContainer}>
-        <img 
-          src="/background_purple_events.png" 
-          alt="Background About Us"
-          className={styles.aboutUsImage}
-        />
-        <div className={styles.textOverlay}>
-          <div className={styles.textContainer}>
-            <p className={styles.normalText}>
-              Estabelecer <span className={styles.highlightText}>identidade</span> e <span className={styles.highlightText}>tradição</span>;
-            </p>
-            <p className={styles.normalText}>
-              Cultivar valores de <span className={styles.highlightText}>união</span> e <span className={styles.highlightText}>colaboração</span>;
-            </p>
-            <p className={styles.normalText}>
-              Fomentar o <span className={styles.highlightText}>espírito</span> <span className={styles.highlightText}>esportivo</span>;
-            </p>
-            <p className={styles.normalText}>
-              Incentivar a <span className={styles.highlightText}>vivência</span> <span className={styles.highlightText}>universitária</span>;
-            </p>
-            <p className={styles.normalText}>
-              Gerar aprendizado sobre <span className={styles.highlightText}>gestão</span> e <span className={styles.highlightText}>trabalho</span> em equipe.
-            </p>
-          </div>
-          <div className={styles.purposeTitle}>Propósito</div>
-
+        <div className={styles["container-mascot__content"]}>
+          <Content 
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec aliquet sem. Morbi volutpat neque sed auctor elementum. Donec justo magna, porttitor in sagittis id, malesuada ac est. Aenean congue metus sed mauris pretium, vitae hendrerit nunc tincidunt.
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec aliquet sem. Morbi volutpat neque sed auctor elementum. Donec justo magna, porttitor in sagittis id, malesuada ac est. Aenean congue metus sed mauris pretium, vitae hendrerit nunc tincidunt."
+          />
         </div>
-      </div>
+      </section>
 
-      <HorizontalSubtitle
-          title="Diretorias"
-          colorImage="red"
-          titleSize="3rem"
-          className={styles.horizontalSubtitle}
-        />
-      <div className={styles.directorSection}>
-        <VerticalSubtitle 
-          title="Diretoria"
-          imageBackground="red"
-          subtitle="presidência"
-        />
-        <div className={styles.fullBoardCardContainer}>
-          {[...Array(6)].map((_, index) => (
-            <FullBoardCard 
-              key={index} 
-              nameBoard={`Nome ${index + 1}`} 
-              description="Integer ultrices elementum mauris nec tincidunt. Mauris et lectus vel nulla condimentum dapibus. Praesent " 
-              linkButton={`/diretoria/${index + 1}`}
-              className={styles.fullBoardCard}
-            />
-          ))}
-        </div>
-      </div>
-      <HorizontalSubtitle 
-        title="Mascote"
-        titleSize="3rem"
-        colorImage="purple"
-      />
-      <div className={styles.mascotContainer}>
-        <img 
-          src="/mascot.png" 
-          alt="Mascote"
-          className={styles.mascotImage}
-        />
-      </div>
-
-      <div className={styles.centeredContentContainer}>
-        <Content 
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec aliquet sem. Morbi volutpat neque sed auctor elementum. Donec justo magna, porttitor in sagittis id, malesuada ac est. Aenean congue metus sed mauris pretium, vitae hendrerit nunc tincidunt.
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec aliquet sem. Morbi volutpat neque sed auctor elementum. Donec justo magna, porttitor in sagittis id, malesuada ac est. Aenean congue metus sed mauris pretium, vitae hendrerit nunc tincidunt."
-          className={styles.centeredContent}
-        />
-      </div>
-      <div className={styles.centeredContentForm}>
-      <div className={styles.tag} />
-      <FormCard 
+      <section className={styles.section}>
+        <div className={styles["container-form"]}>
+          <div className={styles["container-form__tag"]} />
+          <FormCard 
             title="Entre em Contato" 
             inputs={inputs} 
             textButton="Enviar"
@@ -124,7 +136,8 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec aliquet sem. Mor
             backgroundType="purple"
             inputStyle="white"
           />
-      </div>
+        </div>
+      </section>
     </>
   );
 }

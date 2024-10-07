bbf0417296bb351ec00a6e9aa6925592b9caa75e
Anag0es <analuisa.goes2004@gmail.com>
2024-10-07 11:39:34 -0300

    feat: event-announcement integration

--- src/frontend/src/pages/AboutEvent/index.jsx
+++ src/frontend/src/pages/AboutEvent/index.jsx
@@ -1,4 +1,7 @@
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import dayjs from 'dayjs';
 import MainTitle from "../../components/MainTitle";
 import VerticalSubtitle from "../../components/VerticalSubtitle";
 import HorizontalSubtitle from "../../components/HorizontalSubtitle";
@@ -8,62 +11,51 @@ import Button from "../../components/Buttons/Button";
 import styles from "./styles.module.scss";
 import PaperBackground from "../../components/PaperBackground";
 
-const events = [
-  {
-    id: 1,
-    name: "Festa Anterior 1",
-    description: "Descrição completa da Festa Anterior 1",
-    fullImage: "/partner-furioso.png",
-    address: "Rua Alegria, 456",
-    date: "01/01/2023",
-    ticket: "R$ 50",
-    observation: "Evento encerrado.",
-  },
-  {
-    id: 2,
-    name: "Festa Anterior 2",
-    description: "Descrição completa da Festa Anterior 2",
-    fullImage: "/partner-furioso.png",
-    address: "Rua Diversão, 789",
-    date: "02/02/2023",
-    ticket: "R$ 60",
-    observation: "Evento encerrado.",
-  },
-  {
-    id: 3,
-    name: "Festa Anterior 3",
-    description: "Descrição completa da Festa Anterior 3",
-    fullImage: "/partner-furioso.png",
-    address: "Rua Festa, 101",
-    date: "03/03/2023",
-    ticket: "R$ 70",
-    observation: "Evento encerrado.",
-  },
-  {
-    id: 4,
-    name: "O Covil",
-    description: "Nova festa da Tan Tan",
-    fullImage: "/partner-furioso-full.png",
-    address: "Rua Furiosa, 123",
-    date: "10/09/2023",
-    ticket: "R$ 50",
-    observation: "Entrada permitida apenas para maiores de 18 anos.",
-  },
-  {
-    id: 5,
-    name: "Campeonato de basquete",
-    description: "Novo campeonato de basquete",
-    fullImage: "/partner-furioso-full.png",
-    address: "Rua Furiosa, 123",
-    date: "10/09/2023",
-    ticket: "R$ 70",
-    observation: "Participação gratuita para sócios.",
-  },
-];
-
 export default function AboutEvent() {
   const { id } = useParams();
-  const event = events.find((event) => event.id === parseInt(id));
+  const [event, setEvent] = useState(null);
+
+
+  useEffect(() => {
+    // rota do backend
+    const fetchEvent = async () => {
+      try {
+        const response = await axios.get(
+          `http://54.145.177.228:1337/api/events/${id}?populate=*`
+        );
+
+        const eventData = response.data.data;
+
+        const formattedDates = [
+          {
+            date: dayjs(eventData.attributes.start_time).format('DD/MM/YYYY'),
+            startHour: dayjs(eventData.attributes.start_time).format('HH:mm'),
+            endHour: dayjs(eventData.attributes.end_time).format('HH:mm'),
+          },
+        ];
+
+        // Mapeamento dos atributos
+        const mappedEvent = {
+          id: eventData.id,
+          name: eventData.attributes.title,
+          description: eventData.attributes.description,
+          fullImage:
+            eventData?.attributes?.image?.data?.[0]?.attributes?.url || "",
+          address: `${eventData.attributes.street}, ${eventData.attributes.number}, ${eventData.attributes.postal_code}`,
+          dates: formattedDates,          
+          ticket: `R$ ${eventData.attributes.price}`,
+          observation: eventData.attributes.note,
+        };
+
+        setEvent(mappedEvent);
+      } catch (error) {
+        console.error("Erro ao buscar o evento:", error);
+      }
+    };
+
+    fetchEvent();
+  }, [id]);
+
 
   if (!event) {
     return <p>Evento não encontrado.</p>;
@@ -77,24 +69,33 @@ export default function AboutEvent() {
           <VerticalSubtitle
             title="Sobre o evento"
             imageBackground="purple"
-            className={`${styles["container--title__vertical-subtitle"]}`}
+            className={styles["container--title__vertical-subtitle"]}
           />
           <HorizontalSubtitle
             title="Sobre o evento"
             colorImage="purple"
             titleSize="3rem"
-            className={styles["container--title__horizontal-subtitle"]} 
+            className={styles["container--title__horizontal-subtitle"]}
           />
 
           <div className={styles["container--event"]}>
             <Content content={event.description} />
             <div className={styles["container--button"]}>
-              <Button title='Participar' path={`/eventos/${event.id}/inscricao`} />
+              <Button
+                title="Participar"
+                path={`/eventos/${event.id}/inscricao`}
+              />
             </div>
           </div>
         </section>
         <section className={styles["container--infos"]}>
-          <EventInformationCard address={event.address} dates={[{ date: event.date, startHour: '08:00', endHour: '12:00' }]} observation={event.observation} image={event.fullImage} />
+          <EventInformationCard
+            address={event.address}
+            // Passa a string formatada diretamente para o componente
+            dates={[{ date: event.formattedDates }]}
+            observation={event.observation}
+            image={event.fullImage}
+          />
         </section>
       </PaperBackground>
     </>
